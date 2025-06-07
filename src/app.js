72c71ce99a3bcc6a2900fb5429c8d9ddcf86e453
danendra.wyantam <danendra.wyantam@neuronworks.co.id>
2025-06-07 19:53:29 +0700

    coba push ulang

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const createServer = require('./Infrastructures/http/createServer');
 const container = require('./Infrastructures/container');
  
+
 const start = async () => {
   const server = await createServer(container);
   await server.start();
